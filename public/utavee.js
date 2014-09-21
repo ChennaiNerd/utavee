57bae9b03ecb78732f7169068ff3859847e4e5d2
Fizer Khan <fizerkhan@gmail.com>
2014-09-21 14:14:29 +0530

    updated sample client

--- public/utavee.js
+++ public/utavee.js
@@ -9,18 +9,26 @@
             _id = obj.id;
             _name = obj.name;
             _email = obj.email;
+
+            var params = "token=" + _token + "&name=" + _name + "&email=" +_email + "&id=" + _id;
+            var link = "http://localhost:3000/widget.html?"+params;
+            var iframe = document.createElement('iframe');
+            iframe.frameBorder=0;
+            iframe.setAttribute("src", link);
+            iframe.setAttribute("style", "position:fixed; bottom:0; right: 50px; width: 512px; height: 600px;");
+            document.getElementsByTagName("body")[0].appendChild(iframe);
         }
     };
 
     // Load iframe
     window.onload = function(){
-        var params = "token=" + _token + "&name=" + _name + "&email=" +_email + "&id=" + _id;
-        var link = "http://localhost:3000/widget.html?"+params;
-        var iframe = document.createElement('iframe');
-        iframe.frameBorder=0;
-        iframe.setAttribute("src", link);
-        iframe.setAttribute("style", "position:absolute; bottom:0; right: 50; width: 512px; height: 600px;");
-        document.getElementsByTagName("body")[0].appendChild(iframe);
+        // var params = "token=" + _token + "&name=" + _name + "&email=" +_email + "&id=" + _id;
+        // var link = "http://localhost:3000/widget.html?"+params;
+        // var iframe = document.createElement('iframe');
+        // iframe.frameBorder=0;
+        // iframe.setAttribute("src", link);
+        // iframe.setAttribute("style", "position:fixed; bottom:0; right: 50; width: 512px; height: 600px;");
+        // document.getElementsByTagName("body")[0].appendChild(iframe);
     }
     window.utavee = utavee;
 
